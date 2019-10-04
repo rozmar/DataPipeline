f5dfd55b56c00317f23d31c8e2cf1eb7f1ea523d
rozmar <qtyush@gmail.com>
2019-10-04 18:02:33 -0400

    lab metadata added

--- notebook_google/notebook_main.py
+++ notebook_google/notebook_main.py
@@ -15,6 +15,16 @@ client = gspread.authorize(creds)
 creds_drive = ServiceAccountCredentials.from_json_keyfile_name('./creds/online_notebook_drive_api.json', scope)
 
 #%% open 
+
+def fetch_lastmodify_time(spreadsheetname):
+    modifiedtime = None
+    ID = None
+    service = build('drive', 'v3', credentials=creds)
+    wb = client.open(spreadsheetname)
+    ID = wb.id
+    if ID:
+        modifiedtime = service.files().get(fileId = ID,fields = 'modifiedTime').execute()
+    return modifiedtime
 def fetch_animal_metadata():
     #%%
     wb = client.open("Surgery, water restriction and training")
@@ -57,14 +67,51 @@ def fetch_water_restriction_metadata(ID):
         return None
 
 def fetch_lastmodify_time_animal_metadata():
-    modifiedtime = None
-    ID = None
-    service = build('drive', 'v3', credentials=creds)
-    wb = client.open("Surgery, water restriction and training")
-    ID = wb.id
-    if ID:
-        modifiedtime = service.files().get(fileId = ID,fields = 'modifiedTime').execute()
-    return modifiedtime
+    return fetch_lastmodify_time('Surgery, water restriction and training')
+
+def fetch_lastmodify_time_lab_metadata():
+    return fetch_lastmodify_time('Lab metadata')
+
+
+def fetch_lab_metadata(ID):
+    #%%
+    wb = client.open("Lab metadata")
+    sheetnames = list()
+    worksheets = wb.worksheets()
+    for sheet in worksheets:
+        sheetnames.append(sheet.title)
+        #%%
+    if ID in sheetnames:
+        idx_now = sheetnames.index(ID)
+        if idx_now > -1:
+            params = {'majorDimension':'ROWS'}
+            temp = wb.values_get(ID+'!A1:O100',params)
+            temp = temp['values']
+            header = temp.pop(0)
+            data = list()
+            for row in temp:
+                if len(row) < len(header):
+                    row.append('')
+                if len(row) == len(header):
+                    data.append(row)
+            df = pd.DataFrame(data, columns = header)
+            return df
+        else:
+            return None
+    else:
+        return None
+# =============================================================================
+#     modifiedtime = None
+#     ID = None
+#     service = build('drive', 'v3', credentials=creds)
+#     wb = client.open("Surgery, water restriction and training")
+#     ID = wb.id
+#     if ID:
+#         modifiedtime = service.files().get(fileId = ID,fields = 'modifiedTime').execute()
+#     return modifiedtime
+# =============================================================================
+
+
 
 #%%
 
