612ad3815c545f442372db9983f4ec79cb3ee0c2
rozmar <qtyush@gmail.com>
2020-03-27 18:48:26 -0400

    SNratio plots

--- plot/plot_imaging.py
+++ plot/plot_imaging.py
@@ -3,6 +3,7 @@ from pipeline import pipeline_tools
 from pipeline import lab, experiment, ephys_patch, ephysanal, imaging, imaging_gt
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib import cm
 def plot_precision_recall(key_cell,binwidth =  30,frbinwidth = 0.01,firing_rate_window = 3,save_figures = True,xlimits =None):
     #%%
     session_time, cell_recording_start = (experiment.Session()*ephys_patch.Cell()&key_cell).fetch1('session_time','cell_recording_start')
@@ -112,13 +113,27 @@ def plot_precision_recall(key_cell,binwidth =  30,frbinwidth = 0.01,firing_rate_
     #%%
 def plot_ephys_ophys_trace(key_cell,time_to_plot=None,trace_window = 1,show_stimulus = False,show_e_ap_peaks = False, show_o_ap_peaks = False):
     #%%
+# =============================================================================
+#     key_cell = {'session': 1,
+#                 'subject_id': 456462,
+#                 'cell_number': 3,
+#                 'motion_correction_method': 'VolPy',
+#                 'roi_type': 'VolPy'}
+#     time_to_plot=None
+#     trace_window = 100
+#     show_stimulus = False
+#     show_e_ap_peaks = True
+#     show_o_ap_peaks = True
+# =============================================================================
+    
+    
     fig=plt.figure()
     ax_ophys = fig.add_axes([0,0,2,.8])
     ax_ephys = fig.add_axes([0,-1,2,.8])
     if show_stimulus:
         ax_ephys_stim = fig.add_axes([0,-1.5,2,.4])
         
-    #%%
+    #%
     session_time, cell_recording_start = (experiment.Session()*ephys_patch.Cell()&key_cell).fetch1('session_time','cell_recording_start')
     first_movie_start_time =  np.min(np.asarray(((imaging.Movie()*imaging_gt.GroundTruthROI())&key_cell).fetch('movie_start_time'),float))
     first_movie_start_time_real = first_movie_start_time + session_time.total_seconds()
@@ -126,11 +141,11 @@ def plot_ephys_ophys_trace(key_cell,time_to_plot=None,trace_window = 1,show_stim
         time_to_plot = trace_window/2#ephys_matched_ap_times[0]
     session_time_to_plot = time_to_plot+first_movie_start_time  # time relative to session start
     cell_time_to_plot= session_time_to_plot + session_time.total_seconds() -cell_recording_start.total_seconds() # time relative to recording start
-    #%%
+    #%
     sweep_start_times,sweep_end_times,sweep_nums = (ephys_patch.Sweep()&key_cell).fetch('sweep_start_time','sweep_end_time','sweep_number')
     needed_start_time = cell_time_to_plot - trace_window/2
     needed_end_time = cell_time_to_plot + trace_window/2
-    #%%
+    #%
     sweep_nums = sweep_nums[((sweep_start_times > needed_start_time) & (sweep_start_times < needed_end_time)) |
                             ((sweep_end_times > needed_start_time) & (sweep_end_times < needed_end_time)) | 
                             ((sweep_end_times > needed_end_time) & (sweep_start_times < needed_start_time)) ]
@@ -192,10 +207,20 @@ def plot_ephys_ophys_trace(key_cell,time_to_plot=None,trace_window = 1,show_stim
         key_movie = key_cell.copy()
         key_movie['movie_number'] = movie_num
 
-        dff = ((imaging.ROI()*imaging_gt.GroundTruthROI())&key_movie).fetch1('roi_dff')
+        dff,gt_roi_number = ((imaging.ROI()*imaging_gt.GroundTruthROI())&key_movie).fetch1('roi_dff','roi_number')
+        dff_all,roi_number_all = (imaging.ROI()&key_movie).fetch('roi_dff','roi_number')
+        dff_all =dff_all[roi_number_all!=gt_roi_number]
         frame_times = ((imaging.MovieFrameTimes()*imaging_gt.GroundTruthROI())&key_movie).fetch1('frame_times') + (session_time).total_seconds() - first_movie_start_time_real #modified to cell time
         frame_idx = (time_to_plot-trace_window/2 < frame_times) & (time_to_plot+trace_window/2 > frame_times)
-        ax_ophys.plot(frame_times[frame_idx],dff[frame_idx],'g-')
+        
+        dff_list = [dff]
+        dff_list.extend(dff_all)
+        prevminval = 0
+        for dff_now,alpha_now in zip(dff_list,np.arange(1,1/(len(dff_list)+1),-1/(len(dff_list)+1))):
+            dfftoplotnow = dff_now[frame_idx] + prevminval
+            ax_ophys.plot(frame_times[frame_idx],dfftoplotnow,'g-',alpha=alpha_now)
+            prevminval = np.min(dfftoplotnow) -.005
+        #ax_ophys.plot(frame_times[frame_idx],dff[frame_idx],'g-')
         dffs.append(dff)
         frametimes.append(frame_times)
         if show_o_ap_peaks:
@@ -242,7 +267,7 @@ def plot_ephys_ophys_trace(key_cell,time_to_plot=None,trace_window = 1,show_stim
     else:
         ax_ephys.set_xlabel('time from first movie start (s)')
     outdict = {'ephys':ephysdata,'ophys':ophysdata,'figure_handle':fig}
-
+#%%
     return outdict
 
 
@@ -297,7 +322,7 @@ def plot_IV(subject_id = 454597, cellnum = 1, ivnum = 0,IVsweepstoplot = None):
     #ax_stim.set_title('Stimulus')
     return fig
 
-def plot_AP_waveforms(key,AP_tlimits = [-.005,.01]):
+def plot_AP_waveforms(key,AP_tlimits = [-.005,.01],save_image = False):
     #%
     select_high_sn_APs = False
     bin_step = .00001
@@ -310,7 +335,7 @@ def plot_AP_waveforms(key,AP_tlimits = [-.005,.01]):
     tau_2_off = 3.9/1000
     tau_1_ratio_off = 55
     #%
-    movie_numbers,sweep_numbers,apwavetimes,apwaves,famerates,snratio,apnums = ((imaging_gt.GroundTruthROI()*imaging.Movie()*imaging_gt.ROIAPWave())&key).fetch('movie_number','sweep_number','apwave_time','apwave_dff','movie_frame_rate','apwave_snratio','ap_num')
+    movie_numbers,sweep_numbers,apwavetimes,apwaves,famerates,snratio,apnums,ap_threshold = ((imaging_gt.GroundTruthROI()*imaging.Movie()*imaging_gt.ROIAPWave()*ephysanal.ActionPotentialDetails())&key&'ap_real = 1').fetch('movie_number','sweep_number','apwave_time','apwave_dff','movie_frame_rate','apwave_snratio','ap_num','ap_threshold')
     uniquemovienumbers = np.unique(movie_numbers)
     for movie_number in uniquemovienumbers:
         
@@ -393,6 +418,144 @@ def plot_AP_waveforms(key,AP_tlimits = [-.005,.01]):
         ax_e_convolved.legend()
         ax_e_convolved.set_xlim(np.asarray(AP_tlimits)*1000)
         ax_e_convolved.set_xlabel('ms')
+        plt.show()
         imaging_gt.ROIEphysCorrelation()
-        fig.savefig('./figures/APwaveforms_subject_{}_cell_{}_movie_{}.png'.format(key['subject_id'],key['cell_number'],movie_number), bbox_inches = 'tight')  
+        if save_image:
+            fig.savefig('./figures/APwaveforms_subject_{}_cell_{}_movie_{}.png'.format(key['subject_id'],key['cell_number'],movie_number), bbox_inches = 'tight')  
         #break
+        
+
+def plot_cell_SN_ratio_APwise(roi_type = 'VolPy',v0_max = -35,holding_min = -600,bin_num = 10 ):       
+    #%% Show S/N ratios for each AP
+    cmap = cm.get_cmap('jet')
+# =============================================================================
+#     bin_num = 10
+#     holding_min = -600 #pA
+#     v0_max = -35 #mV
+#     roi_type = 'Spikepursuit'#'Spikepursuit'#'VolPy_denoised'#'SpikePursuit'#'VolPy_dexpF0'#'VolPy'#'SpikePursuit_dexpF0'#'VolPy_dexpF0'#''Spikepursuit'#'VolPy'#
+# =============================================================================
+    key = {'roi_type':roi_type}
+    gtdata = pd.DataFrame((imaging_gt.GroundTruthROI()&key))
+    cells = gtdata.groupby(['session', 'subject_id','cell_number','motion_correction_method','roi_type']).size().reset_index(name='Freq')
+    snratio = list()
+    v0s = list()
+    holdings = list()
+    rss = list()
+    threshs =list()
+    mintreshs = list()
+    f0s_all = list()
+    snratios_all = list()
+    peakamplitudes_all = list()
+    noise_all = list()
+    for cell in cells.iterrows():
+        cell = cell[1]
+        key_cell = dict(cell)    
+        del key_cell['Freq']
+        snratios,f0,peakamplitudes,noises = (imaging_gt.GroundTruthROI()*imaging_gt.ROIAPWave()*ephysanal.ActionPotentialDetails()&key_cell&'ap_real = 1').fetch('apwave_snratio','apwave_f0','apwave_peak_amplitude','apwave_noise')
+        #f0 =  (imaging_gt.GroundTruthROI()*imaging.ROI()&key_cell).fetch('roi_f0')
+        
+        sweep = (imaging_gt.GroundTruthROI()*imaging_gt.ROIAPWave()&key_cell).fetch('sweep_number')[0]
+        thresh = (imaging_gt.GroundTruthROI()*imaging_gt.ROIAPWave()*ephysanal.ActionPotentialDetails()&key_cell&'ap_real = 1').fetch('ap_threshold')
+        trace = (ephys_patch.SweepResponse()*imaging_gt.GroundTruthROI()&key_cell&'sweep_number = {}'.format(sweep)).fetch('response_trace')
+        trace =trace[0]
+        stimulus = (ephys_patch.SweepStimulus()*imaging_gt.GroundTruthROI()&key_cell&'sweep_number = {}'.format(sweep)).fetch('stimulus_trace')
+        stimulus =stimulus[0]
+        
+        RS = (ephysanal.SweepSeriesResistance()*imaging_gt.GroundTruthROI()&key_cell&'sweep_number = {}'.format(sweep)).fetch('series_resistance')
+        RS =RS[0]
+        
+        medianvoltage = np.median(trace)*1000
+        holding = np.median(stimulus)*10**12
+        #print(np.mean(snratios[:100]))    
+        snratio.append(np.mean(snratios[:50]))
+        v0s.append(medianvoltage)
+        holdings.append(holding)
+        rss.append(RS)
+        threshs.append(thresh)
+        mintreshs.append(np.min(thresh))
+        f0s_all.append(f0)
+        snratios_all.append(snratios)
+        peakamplitudes_all.append(peakamplitudes)
+        noise_all.append(noises)
+        #plot_AP_waveforms(key_cell,AP_tlimits)
+    
+    #%%  for each AP
+    
+    fig=plt.figure()
+    ax_sn_f0 = fig.add_axes([0,0,1,1])
+    ax_sn_f0_binned = fig.add_axes([0,-1.2,1,1])
+    ax_noise_f0 = fig.add_axes([2.6,0,1,1])
+    ax_noise_f0_binned = fig.add_axes([2.6,-1.2,1,1])
+    ax_peakampl_f0 = fig.add_axes([1.3,0,1,1])
+    ax_peakampl_f0_binned = fig.add_axes([1.3,-1.2,1,1])
+    for loopidx, (f0,snratio_now,noise_now,peakampl_now,cell_now) in enumerate(zip(f0s_all,snratios_all,noise_all,peakamplitudes_all,cells.iterrows())):
+        coloridx = loopidx/len(cells)
+        cell_now = cell_now[1]
+        label_now = 'Subject:{}'.format(cell_now['subject_id'])+' Cell:{}'.format(cell_now['cell_number'])
+        ax_sn_f0.plot(f0,snratio_now,'o',ms=1, color = cmap(coloridx), label= label_now)
+        ax_noise_f0.plot(f0,noise_now,'o',ms=1, color = cmap(coloridx), label= label_now)
+        ax_peakampl_f0.plot(f0,peakampl_now,'o',ms=1, color = cmap(coloridx), label= label_now)
+        lows = np.arange(np.min(f0),np.max(f0),(np.max(f0)-np.min(f0))/(bin_num+1))
+        highs = lows + (np.max(f0)-np.min(f0))/(bin_num+1)
+        mean_f0 = list()
+        sd_f0 = list()
+        mean_sn = list()
+        sd_sn =list()
+        mean_noise = list()
+        sd_noise =list()
+        mean_ampl = list()
+        sd_ampl =list()
+        for low,high in zip(lows,highs):
+            idx = (f0 >= low) & (f0 < high)
+            if len(idx)>10:
+                mean_f0.append(np.mean(f0[idx]))
+                sd_f0.append(np.std(f0[idx]))
+                mean_sn.append(np.mean(snratio_now[idx]))
+                sd_sn.append(np.std(snratio_now[idx]))
+                mean_noise.append(np.mean(noise_now[idx]))
+                sd_noise.append(np.std(noise_now[idx]))
+                mean_ampl.append(np.mean(peakampl_now[idx]))
+                sd_ampl.append(np.std(peakampl_now[idx]))
+                
+        ax_sn_f0_binned.errorbar(mean_f0,mean_sn,sd_sn,sd_f0,'o-', color = cmap(coloridx), label= label_now)
+        ax_noise_f0_binned.errorbar(mean_f0,mean_noise,sd_noise,sd_f0,'o-', color = cmap(coloridx), label= label_now)
+        ax_peakampl_f0_binned.errorbar(mean_f0,mean_ampl,sd_ampl,sd_f0,'o-', color = cmap(coloridx), label= label_now)
+            
+    ax_sn_f0.set_xlabel('F0')
+    ax_sn_f0.set_ylabel('S/N ratio')
+    ax_sn_f0_binned.set_xlabel('F0')
+    ax_sn_f0_binned.set_ylabel('S/N ratio')
+    
+    #ax_sn_f0_binned.legend()
+    ax_sn_f0_binned.legend(loc='upper center', bbox_to_anchor=(-.35, 1.5), shadow=True, ncol=1)
+
+    
+    
+    ax_noise_f0.set_xlabel('F0')
+    ax_noise_f0.set_ylabel('Noise (std(dF/F))')
+    ax_noise_f0_binned.set_xlabel('F0')
+    ax_noise_f0_binned.set_ylabel('Noise (std(dF/F))')
+    
+    ax_peakampl_f0.set_xlabel('F0')
+    ax_peakampl_f0.set_ylabel('Peak amplitude (dF/F)')
+    ax_peakampl_f0_binned.set_xlabel('F0')
+    ax_peakampl_f0_binned.set_ylabel('Peak amplitude (dF/F)')
+    
+    #%%
+    cells['SN']=snratio
+    cells['V0']=v0s
+    cells['holding']=holdings
+    cells['RS']=np.asarray(rss,float)
+    print(cells)
+    cells = cells[cells['V0']<v0_max]
+    cells = cells[cells['holding']>holding_min]
+    print(cells)
+    #% S/N ratio histogram
+    fig=plt.figure()
+    ax_hist = fig.add_axes([0,0,1,1])
+    ax_hist.hist(cells['SN'].values)
+    ax_hist.set_xlabel('S/N ratio of first 50 spikes')
+    ax_hist.set_ylabel('# of cells')
+    ax_hist.set_title(roi_type.replace('_',' '))
+    ax_hist.set_xlim([0,15])
+    #fig.savefig('./figures/SN_hist_{}.png'.format(roi_type), bbox_inches = 'tight')
